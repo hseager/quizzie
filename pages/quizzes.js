08811b8c3b008bc082cf1691a62735a905c243d6
hseager <harvey.seager@googlemail.com>
2020-08-10 16:26:35 +0100

    Added MongoDB

--- pages/quizzes.js
+++ pages/quizzes.js
@@ -1,11 +1,11 @@
 import Layout from '../components/layout'
 import Link from 'next/link'
-import { getQuizzes } from '../libs/quizzes'
 import styles from '../styles/quizzes.module.css'
 import { getUserId } from '../libs/localStorage'
 import { useState, useEffect } from 'react'
+import fetch from 'isomorphic-unfetch'
 
-export default function Quizzes({ quizzes }) {
+const Quizzes = function({ quizzes }) {
 
     const [userId, setUserId] = useState()
     
@@ -18,7 +18,7 @@ export default function Quizzes({ quizzes }) {
             <h1>Quizzes</h1>
             <div className={styles.quizList}>
                 {quizzes.map((quiz) => (
-                    <div className={styles.quizListItem} key={quiz.id}>
+                    <div className={styles.quizListItem} key={quiz._id}>
                         <h6 className={styles.quizCategory}>{quiz.category}</h6>
                         <h4 className={styles.quizName}>
                             <Link href='/quiz/[slug]' as={`/quiz/${quiz.slug}?lid=${userId}`}>
@@ -33,10 +33,16 @@ export default function Quizzes({ quizzes }) {
 }
 
 export async function getStaticProps() {
-    const quizzes = getQuizzes()
+
+    const res = await fetch("http://localhost:3000/api/quizzes")
+    const json = await res.json()
+
     return {
         props: {
-            quizzes,
+            quizzes: json
         }
     }
 }
+
+
+export default Quizzes
