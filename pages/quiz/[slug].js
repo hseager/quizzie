9a685dabe0dccc1c6c8094d27333d249acbcae08
hseager <harvey.seager@googlemail.com>
2020-11-13 16:43:02 +0000

    Added loading to quiz page

--- pages/quiz/[slug].js
+++ pages/quiz/[slug].js
@@ -3,11 +3,16 @@ import buttonStyles from '../../styles/buttons.module.css'
 import { getUserId } from '../../libs/localStorage'
 import fetch from 'isomorphic-unfetch'
 import Router from 'next/router'
+import { useState } from 'react'
+import pageStyles from '../../styles/page.module.css'
 
 export default function Quiz({ quiz }) {
 
+    const [loading, setLoading] = useState(false)
+
     const createLobby = () => {
         // TODO: show loading gif etc
+        setLoading(true)
         fetch(`${process.env.NEXT_PUBLIC_HOST}/api/lobbies/createOrUpdate`, {
             method: 'post',
             body: JSON.stringify({ 
@@ -23,17 +28,29 @@ export default function Quiz({ quiz }) {
 
     return (
         <Layout>
-            <h2>{ quiz.name } Quiz</h2>
-            <p>
-                Category:
-            {
-                quiz.category.map( (cat, i) => (
-                    <strong key={i}> {cat}</strong>
-                ))
-            }
-            </p>
-            <p><strong>{ quiz.questions.length }</strong> Questions</p>
-            <button className={buttonStyles.button} onClick={createLobby}>Play Quiz</button>
+            <div className={pageStyles.fullHeightPage}>
+                {   loading &&
+                    <h2>Creating a new lobby...</h2>
+                }
+                {
+                    !loading &&
+                    <>
+                        <h2>{ quiz.name } Quiz</h2>
+                        <p>
+                            Category:
+                        {
+                            quiz.category.map( (cat, i) => (
+                                <strong key={i}> {cat}</strong>
+                            ))
+                        }
+                        </p>
+                        <p><strong>{ quiz.questions.length }</strong> Questions</p>
+                        <button className={buttonStyles.button} onClick={createLobby}>Play Quiz</button>
+                    </>
+                }
+
+            </div>
+
         </Layout>
     )
 
