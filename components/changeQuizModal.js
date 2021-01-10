0d993146d7973103025949d6974a12de514ef194
hseager <harvey.seager@googlemail.com>
2021-01-10 15:12:51 +0000

    Added loader to changeQuizModal

--- components/changeQuizModal.js
+++ components/changeQuizModal.js
@@ -2,8 +2,8 @@ import quizStyles from '../styles/quiz.module.css'
 import Modal from '../components/modal'
 import { useState, useEffect } from 'react'
 import { HttpRequestError } from '../libs/HttpRequestError'
-import QuizImage from '../components/quizImage'
 import QuizCard from '../components/quizCard'
+import LoadingAnimation from '../components/loadingAnimation'
 
 export default function ChangeQuizModal({showModal, setShowModal, changeQuiz}) {
 
@@ -28,35 +28,31 @@ export default function ChangeQuizModal({showModal, setShowModal, changeQuiz}) {
         changeQuiz(quiz)
         setShowModal(false)
     }
-
-    if(!quizzes) {
-        return (
-            <Modal showModal={showModal} setShowModal={setShowModal}>
-                <p>Loading</p>
-            </Modal>
-        )
-    }
-
-    if(quizzes.error) {
-        return (
-            <Modal showModal={showModal} setShowModal={setShowModal}>
-                <h3>{quizzes.title}</h3>
-                <p>{quizzes.message}</p>
-            </Modal>
-        )
-    }
         
     return (
         <Modal showModal={showModal} setShowModal={setShowModal}>
-            <h2>Change Quiz</h2>
-            <div className={quizStyles.list}>
-                {quizzes.map(quiz => (
-                    <QuizCard 
-                        key={quiz._id}
-                        quiz={quiz} 
-                        click={() => onChangeQuiz(quiz)} />
-                ))}
-            </div>
+            { !quizzes && <LoadingAnimation /> }
+            {
+                quizzes && quizzes.error && 
+                <>
+                    <h3>{quizzes.title}</h3>
+                    <p>{quizzes.message}</p>
+                </>
+            }
+            {
+                quizzes && !quizzes.error && 
+                <>
+                    <h2>Change Quiz</h2>
+                    <div className={quizStyles.list}>
+                        {quizzes.map(quiz => (
+                            <QuizCard 
+                                key={quiz._id}
+                                quiz={quiz} 
+                                click={() => onChangeQuiz(quiz)} />
+                        ))}
+                    </div>
+                </>
+            }
         </Modal>
     )
 }
