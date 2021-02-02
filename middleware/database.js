664083ee44bcccfa3d68cacfb4ba04d56df5ad12
hseager <harvey.seager@googlemail.com>
2021-02-02 15:21:47 +0000

    Updated mongo db connection

--- middleware/database.js
+++ middleware/database.js
@@ -2,32 +2,16 @@ import { MongoClient } from 'mongodb'
 
 const { DB_CONNECTION, DB_NAME } = process.env
 
-let cached = global.mongo
-if(!cached) cached = global.mongo = {}
-
-export async function connectToDatabase() {
-    if (cached.connection) return cached.connection.db
-    if (!cached.promise) {
-      let connection = {}
-      cached.promise = MongoClient.connect(DB_CONNECTION, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-        .then((client) => {
-          connection.client = client
-          return client.db(DB_NAME)
-        })
-        .then((db) => {
-          connection.db = db
-          cached.connection = connection
-          return connection
-        })
-    }
-    await cached.promise
-    return cached.connection.db
-}
+global.mongo = global.mongo || {};
 
 export default async function database(req, res, next) {
-    req.db = await connectToDatabase()
+    if (!global.mongo.client) {
+        global.mongo.client = new MongoClient(DB_CONNECTION, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        await global.mongo.client.connect();
+    }
+    req.db = global.mongo.client.db(DB_NAME);
     return next()
 }
